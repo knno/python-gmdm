0501a727b3464c9b2b33ab3a4f0ef7a915d3d6d6
knno <5490140+knno@users.noreply.github.com>
2024-04-05 23:37:43 +0300

    Push version

--- src/gmdm/cli.py
+++ src/gmdm/cli.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """The GMDM CLI tool for managing dependencies of GameMaker projects and files.
 """
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 
 import argparse
 import logging
